b8325b3e80744290695d3520f3b1a14837629916
ChetanNair <93496359+ChetanNair@users.noreply.github.com>
2023-11-08 00:38:49 -0800

    feat: create simple post endpoint

--- roguespot-orch.go
+++ roguespot-orch.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AP struct {
+	SSID string `json:"ssid"`
+	MAC  string `json:"mac"`
+}
+
+func postAP(c *gin.Context) {
+	var newAP AP
+
+	if err := c.BindJSON(&newAP); err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, newAP)
+}
+
 func main() {
+	router := gin.Default()
+	router.POST("/log", postAP)
+	router.Run()
 }
